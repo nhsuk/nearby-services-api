904cff6ddb8d38d97a938fa0736f33656b3ddfd3
steven hunt <st3v3n.hunt@gmail.com>
2016-11-16 15:46:17 +0000

    :wrench: Add security headers

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const helmet = require('helmet');
 
 const app = express();
+app.use(helmet());
+
 app.port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
--- test/integration/app.js
+++ test/integration/app.js
@@ -7,16 +7,38 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('app', () => {
-  it('should return hello world', (done) => {
-    chai.request(app)
-      .get('/')
+  describe('the root', () => {
+    it('should return an OK response', (done) => {
+      chai.request(app)
+        .get('/')
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+
+    it('should return hello world', (done) => {
+      chai.request(app)
+        .get('/')
+        .end((err, res) => {
+          // eslint-disable-next-line no-unused-expressions
+          expect(res).to.be.json;
+          expect(res.body.message).to.equal('Hello World!');
+          done();
+        });
+    });
+
+    it('should return security headers', (done) => {
+      chai.request(app).get('/')
       .end((err, res) => {
-        expect(err).to.equal(null);
-        expect(res).to.have.status(200);
-        // eslint-disable-next-line no-unused-expressions
-        expect(res).to.be.json;
-        expect(res.body.message).to.equal('Hello World!');
+        expect(res).to.have.header('X-Xss-Protection', '1; mode=block');
+        expect(res).to.have.header('X-Frame-Options', 'SAMEORIGIN');
+        expect(res).to.have.header('X-Content-Type-Options', 'nosniff');
+        expect(res).to.have.header('X-Download-Options', 'noopen');
+        expect(res).to.not.have.header('X-Powered-By');
         done();
       });
+    });
   });
 });
