e8b5d6bffeac4d301bcb3cc7af19c225c699ca9f
steven hunt <st3v3n.hunt@gmail.com>
2017-08-08 18:13:14 +0100

    :black_nib: Log date of request

--- app/lib/getDateTime.js
+++ app/lib/getDateTime.js
@@ -1,8 +1,12 @@
 const moment = require('moment');
+const log = require('../lib/logger');
 // use this instead of moment to control current date for testing purposes
 
 function getDateTime() {
-  return moment(process.env.DATETIME || Date.now());
+  const now = process.env.DATETIME || Date.now();
+  log.debug({ date: now }, 'Date of request');
+
+  return moment(now);
 }
 
 module.exports = getDateTime;
