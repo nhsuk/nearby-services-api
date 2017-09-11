7ff5f660dc51917ee18c292ccd6c2b06a483cce9
steven hunt <st3v3n.hunt@gmail.com>
2017-09-11 17:11:20 +0100

    :hotsprings: Extend test for config

--- test/unit/lib/config.js
+++ test/unit/lib/config.js
@@ -6,5 +6,7 @@ const expect = chai.expect;
 describe('config', () => {
   it('should return pharmacies as the default collection', () => {
     expect(config.es.index).to.be.equal('pharmacies');
+    expect(config.es.host).to.be.equal('es');
+    expect(config.es.port).to.be.equal('9200');
   });
 });
