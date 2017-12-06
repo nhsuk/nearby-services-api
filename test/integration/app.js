20cb67a2e2b9939dfd2d2eb2d48e756c2329dab6
Steven Hunt <st3v3n.hunt@gmail.com>
2017-12-06 16:29:35 +0000

    :astonished: Add test for API object model

--- test/integration/app.js
+++ test/integration/app.js
@@ -69,6 +69,75 @@ describe('app', function test() {
           done();
         });
     });
+
+    it('should return an object containing all required properties for both nearby and open services', (done) => {
+      chai.request(app)
+        .get('/nearby')
+        .query({ latitude: coords.latitude, longitude: coords.longitude })
+        .end((err, res) => {
+          expect(err).to.equal(null);
+          expect(res).to.have.status(200);
+          // eslint-disable-next-line no-unused-expressions
+          expect(res).to.be.json;
+
+          const nearby = res.body.nearby;
+          const open = res.body.open;
+          const results = nearby.concat(open);
+
+          results.forEach((result) => {
+            expect(result.identifierType).to.equal('Pharmacy Contract');
+            expect(result.openingTimes).to.not.be.undefined;
+            expect(result.openingTimes.alterations).to.be.instanceof(Object);
+            expect(result.openingTimes.general).to.be.instanceof(Object);
+            expect(result.openingTimes.general.monday).to.be.instanceof(Array);
+            expect(result.openingTimes.general.tuesday).to.be.instanceof(Array);
+            expect(result.openingTimes.general.wednesday).to.be.instanceof(Array);
+            expect(result.openingTimes.general.thursday).to.be.instanceof(Array);
+            expect(result.openingTimes.general.friday).to.be.instanceof(Array);
+            expect(result.openingTimes.general.saturday).to.be.instanceof(Array);
+            expect(result.openingTimes.general.sunday).to.be.instanceof(Array);
+            expect(result.outOfHours).to.be.a('string');
+            expect(result.hasEps).to.be.a('boolean');
+            expect(result.address).to.be.instanceof(Object);
+            expect(result.address.line1).to.be.a('string');
+            expect(result.address.line2).to.not.be.undefined;
+            expect(result.address.line3).to.be.a('string');
+            expect(result.address.city).to.be.a('string');
+            expect(result.address.county).to.be.a('string');
+            expect(result.address.postcode).to.be.a('string');
+            expect(result.contacts).to.be.instanceof(Object);
+            expect(result.contacts.website).to.be.a('string');
+            expect(result.contacts.additionalContacts).to.be.instanceof(Array);
+            expect(result.contacts.telephoneNumber).to.be.a('string');
+            expect(result.contacts.isNonGeographicPhoneNumber).to.be.a('boolean');
+            expect(result.contacts.phoneChargeMethod).to.be.a('string');
+            expect(result.contacts.phoneChargeAmount).to.not.be.undefined;
+            expect(result.contacts.telephoneExtension).to.not.be.undefined;
+            expect(result.contacts.fax).to.be.a('string');
+            expect(result.contacts.email).to.not.be.undefined;
+            expect(result.directionsInformation).to.not.be.undefined;
+            expect(result.externalProfileUrl).to.not.be.undefined;
+            expect(result.identifier).to.be.a('string');
+            expect(result.name).to.be.a('string');
+            expect(result.organisationType).to.be.a('string');
+            expect(result.parkingInformation).to.not.be.undefined;
+            expect(result.provider).to.be.a('string');
+            expect(result.summary).to.be.a('string');
+            expect(result.links).to.be.instanceof(Array);
+            expect(result.location).to.be.instanceof(Object);
+            expect(result.location.type).to.be.a('string');
+            expect(result.location.coordinates).to.be.instanceof(Array);
+            expect(result.openingTimesAsOffset).to.be.instanceof(Array);
+            expect(result.openingTimesAlterationsAsOffset).to.be.instanceof(Array);
+            expect(result.distanceInMiles).to.be.a('number');
+            expect(result.openingTimesMessage).to.be.a('string');
+            expect(result.isOpen).to.be.a('boolean');
+            expect(result.nextOpen).to.be.a('string');
+          });
+
+          done();
+        });
+    });
   });
 
   describe('nearby error handling', () => {
