2be5d06b1ca32a41005cbf6f6390586d2cfbc2f6
Steven Hunt <st3v3n.hunt@gmail.com>
2018-01-12 11:54:34 +0000

    :top: Add open endpoint for open results
    Nearby endpoint now only returns nearby places. Breaking change to the
    API

--- app/middleware/getServices.js
+++ app/middleware/getServices.js
@@ -1,19 +1,17 @@
 const config = require('../../config/config').result;
-const getNearbyServices = require('../lib/getNearbyServices');
+const getServices = require('../lib/getServices');
 const log = require('../lib/logger');
 
-async function validateRequest(req) {
-  const maxNearby = config.limits.nearby.max;
-  const minNearby = config.limits.nearby.min;
-  const maxOpen = config.limits.open.max;
-  const minOpen = config.limits.open.min;
+async function validateRequest(req, type) {
+  const limits = type === 'open' ? config.limits.open : config.limits.nearby;
+  const min = limits.min;
+  const max = limits.max;
 
   req.checkQuery('latitude', 'latitude is required').notEmpty();
   req.checkQuery('longitude', 'longitude is required').notEmpty();
   req.checkQuery('latitude', 'latitude must be between -90 and 90').isFloat({ min: -90, max: 90 });
   req.checkQuery('longitude', 'longitude must be between -180 and 180').isFloat({ min: -180, max: 180 });
-  req.checkQuery('limits:results:open', `limits:results:open must be a number between ${minOpen} and ${maxOpen}`).optional().isInt({ min: minOpen, max: maxOpen });
-  req.checkQuery('limits:results:nearby', `limits:results:nearby must be a number between ${minNearby} and ${maxNearby}`).optional().isInt({ min: minNearby, max: maxNearby });
+  req.checkQuery('limits:results', `limits:results must be a number between ${min} and ${max}`).optional().isInt({ min, max });
   const result = await req.getValidationResult();
   return result.isEmpty() ? undefined : result.array();
 }
@@ -24,24 +22,25 @@ function getSearchCoordinates(req) {
   return { longitude, latitude };
 }
 
-function getLimits(req) {
-  const nearby = Number(req.query['limits:results:nearby']) || config.defaults.nearby;
-  const open = Number(req.query['limits:results:open']) || config.defaults.open;
+function getLimits(req, type) {
+  const results = Number(req.query['limits:results']) || (type === 'open' ? config.defaults.open : config.defaults.nearby);
   // Given how search performance is impacted by the radius of the search
   // it has intentionaly not been allowed to be specificed by the client
+  // TODO: Check if the search radius can be got rid of
   const searchRadius = 20;
-  return { nearby, open, searchRadius };
+  return { results, searchRadius };
 }
 
-async function getServices(req, res, next) {
-  const errors = await validateRequest(req);
+async function getNearbyServices(req, res, next) {
+  const type = 'nearby';
+  const errors = await validateRequest(req, type);
   if (errors) {
     log.warn(errors, 'Errors found on request.');
     res.status(400).json(errors);
   } else {
     try {
-      const services = await getNearbyServices(getSearchCoordinates(req), getLimits(req));
-      res.json({ nearby: services.nearbyServices, open: services.openServices });
+      const results = await getServices.nearby(getSearchCoordinates(req), getLimits(req, type));
+      res.json({ results });
       next();
     } catch (err) {
       res.status(500).send({ err });
@@ -50,4 +49,25 @@ async function getServices(req, res, next) {
   }
 }
 
-module.exports = getServices;
+async function getOpenServices(req, res, next) {
+  const type = 'open';
+  const errors = await validateRequest(req, type);
+  if (errors) {
+    log.warn(errors, 'Errors found on request.');
+    res.status(400).json(errors);
+  } else {
+    try {
+      const results = await getServices.open(getSearchCoordinates(req), getLimits(req, type));
+      res.json({ results });
+      next();
+    } catch (err) {
+      res.status(500).send({ err });
+      next(err);
+    }
+  }
+}
+
+module.exports = {
+  nearby: getNearbyServices,
+  open: getOpenServices,
+};
